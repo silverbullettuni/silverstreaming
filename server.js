e4215303c59d625684f03524c088b700b12d19fe
Anna Suo-Anttila <anvuori@gmail.com>
2020-10-18 18:01:12 +0300

    serverside changes

--- server.js
+++ server.js
@@ -8,16 +8,47 @@ const httpsPort = 8443;
 const http = require("http");
 var https = require('https');
 var fs = require('fs');
-var privateKey  = fs.readFileSync('sslcert/78633766_18.191.139.1734000.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/78633766_18.191.139.1734000.cert', 'utf8');
+var privateKey  = fs.readFileSync('sslcert/18.191.139.1738443.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/18.191.139.1738443.cert', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
 const server = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
-const io = require("socket.io")(httpsServer);
+const io = require("socket.io")(server);
 app.use(express.static(__dirname + "/public"));
 
 io.sockets.on("error", e => console.log(e));
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+"server.js" 43L, 1473C                                        17,33         Top
+io.sockets.on("connection", socket => {
+    git commit --amend --reset-author
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
+server.listen(port, () => console.log(`Server is running on port ${port}`));
+httpsServer.listen(httpsPort, () => console.log(`Server is running on port ${httpsPort}`));
+"server.js" 43L, 1473C                                        43,33         Bot
 io.sockets.on("connection", socket => {
   socket.on("broadcaster", () => {
     broadcaster = socket.id;
