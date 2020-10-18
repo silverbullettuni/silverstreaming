5dbb4b9d8694f2d1453ec94111fbed5dab51e12e
Anna Suo-Anttila <anvuori@gmail.com>
2020-10-18 18:05:54 +0300

    correct serverside code for server.js

--- server.js
+++ server.js
@@ -22,37 +22,6 @@ io.sockets.on("connection", socket => {
   socket.on("broadcaster", () => {
     broadcaster = socket.id;
     socket.broadcast.emit("broadcaster");
-"server.js" 43L, 1473C                                        17,33         Top
-io.sockets.on("connection", socket => {
-    git commit --amend --reset-author
-io.sockets.on("connection", socket => {
-  socket.on("broadcaster", () => {
-    broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster");
-  });
-  socket.on("watcher", () => {
-    socket.to(broadcaster).emit("watcher", socket.id);
-  });
-  socket.on("offer", (id, message) => {
-    socket.to(id).emit("offer", socket.id, message);
-  });
-  socket.on("answer", (id, message) => {
-    socket.to(id).emit("answer", socket.id, message);
-  });
-  socket.on("candidate", (id, message) => {
-    socket.to(id).emit("candidate", socket.id, message);
-  });
-  socket.on("disconnect", () => {
-    socket.to(broadcaster).emit("disconnectPeer", socket.id);
-  });
-});
-server.listen(port, () => console.log(`Server is running on port ${port}`));
-httpsServer.listen(httpsPort, () => console.log(`Server is running on port ${httpsPort}`));
-"server.js" 43L, 1473C                                        43,33         Bot
-io.sockets.on("connection", socket => {
-  socket.on("broadcaster", () => {
-    broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster");
   });
   socket.on("watcher", () => {
     socket.to(broadcaster).emit("watcher", socket.id);
