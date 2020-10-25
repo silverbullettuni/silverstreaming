67d5b9b95b521fadcef3a532af23ad6035d3eae8
Anna Suo-Anttila <anvuori@gmail.com>
2020-10-25 19:38:41 +0200

    Deploy with docker

--- public/deploy.sh
+++ public/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-docker stop -t node_server
-docker stop -t ui
-docker rm -t node_server
-docker rm -t ui
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 rm -r silverstreaming-deploy-branch
 wget https://github.com/silverbullettuni/silverstreaming/archive/deploy-branch.tar.gz
 tar -zxvf deploy-branch.tar.gz
@@ -10,6 +8,6 @@ rm deploy-branch.tar.gz
 cd silverstreaming-deploy-branch
 docker build --tag node_server . -f Dockerfile_server
 docker build --tag ui . -f Dockerfile_ui
-docker run -d -t node_server
-docker run -d -t ui
+docker run -d -p 4000:4000 -t node_server
+docker run -d -p 3000:3000 -t ui
 
