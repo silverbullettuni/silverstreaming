2e55701f923d0359524f943c3dc701a76a1947f4
Anna Suo-Anttila <anvuori@gmail.com>
2020-10-25 19:13:20 +0200

    docker

--- public/deploy.sh
+++ public/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-kill -9 $(lsof -t -i:4000)
-kill -9 $(lsof -t -i:8443)
+docker stop -t node_server
+docker stop -t ui
+docker rm -t node_server
+docker rm -t ui
 rm -r silverstreaming-deploy-branch
 wget https://github.com/silverbullettuni/silverstreaming/archive/deploy-branch.tar.gz
 tar -zxvf deploy-branch.tar.gz
 rm deploy-branch.tar.gz
 cd silverstreaming-deploy-branch
-npm install
-while /bin/true; do
-    node server
-done &
+docker build --tag node_server . -f Dockerfile_server
+docker build --tag ui . -f Dockerfile_ui
+docker run -d -t node_server
+docker run -d -t ui
 
