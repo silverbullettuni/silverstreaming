8d5a923fb5129f189dd31238bc5591040c7f5c0d
Anna Suo-Anttila <anvuori@gmail.com>
2020-11-01 23:14:32 +0200

    Streamer timeout

--- src/Containers/InfoContainer.js
+++ src/Containers/InfoContainer.js
@@ -1,88 +1,89 @@
-import React, { useState, useEffect, createContext, useRef } from 'react';
-import {useParams} from "react-router-dom";
-import BroadcastContainer from './BroadcastContainer';
-import ViewContainer from './ViewContainer';
+import React, { useState, useEffect, createContext, useRef } from "react";
+import { useParams } from "react-router-dom";
+import BroadcastContainer from "./BroadcastContainer";
+import ViewContainer from "./ViewContainer";
 
 import socketIOClient from "socket.io-client";
 
 export const DataContext = createContext();
- 
-export default function InfoContainer(props){
 
-    const ENDPOINT = window.location.hostname + ":4000";
-    
-    const [uid, setUid] = useState('');
-    const socket = socketIOClient(ENDPOINT);
-    const [participant, setParticipant] = useState('');
+export default function InfoContainer(props) {
+  const ENDPOINT = window.location.hostname + ":4000";
 
-    const [isExit, setIsExit] = useState(false);
+  const [uid, setUid] = useState("");
+  const socket = socketIOClient(ENDPOINT);
+  const [participant, setParticipant] = useState("");
 
-    const {wb} = useParams();
- 
-    // Variable for all data
-    const data = {
-        participant:participant,
-        uid:uid
+  const [isExit, setIsExit] = useState(false);
+
+  const { wb } = useParams();
+
+  // Variable for all data
+  const data = {
+    participant: participant,
+    uid: uid,
+  };
+
+  useEffect(() => {
+    inputUsername();
+    window.onbeforeunload = function () {
+      setIsExit(true);
+      socket.emit("exitChatbox");
+      if (wb == "broadcast") {
+        socket.emit("streamerTimeout");
+      }
     };
-    
-    
+  }, [isExit]);
 
-    useEffect(() => {
-        inputUsername();
-        window.onbeforeunload = function () {
-            setIsExit(true);
-            socket.emit('exitChatbox')
-        }
-    },[isExit])
- 
-    function generateUid() {
-        return new Date().getTime() + "" + Math.floor(Math.random()*9+1)
+  function generateUid() {
+    return new Date().getTime() + "" + Math.floor(Math.random() * 9 + 1);
+  }
+
+  function inputUsername() {
+    if (window.sessionStorage.getItem("userData") === null) {
+      let participant = window.prompt("Please enter your username ");
+      let uid = generateUid();
+      if (!participant) {
+        participant = "guest" + uid;
+      }
+      setParticipant(participant);
+      setUid(uid);
+
+      var obj = { uid: uid, username: participant };
+      var str = JSON.stringify(obj);
+
+      window.sessionStorage.setItem("userData", str);
+
+      if (participant != "") {
+        socket.emit("login", { uid: uid, username: participant });
+      }
+    } else {
+      let getUser = window.sessionStorage.getItem("userData");
+      var obj = JSON.parse(getUser);
+      setUid(obj.uid);
+      setParticipant(obj.username);
+      if (participant != "") {
+        socket.emit("login", { uid: uid, username: participant });
+      }
     }
+  }
 
-    function inputUsername(){
-        if (window.sessionStorage.getItem('userData') === null){
-            let participant = window.prompt('Please enter your username ');
-            let uid = generateUid();
-            if (!participant){ participant =  'guest' + uid}
-            setParticipant(participant);
-            setUid(uid);
-
-            var obj = {uid:uid, username:participant}
-            var str = JSON.stringify(obj)
-            
-            window.sessionStorage.setItem('userData', str)
-
-            if(participant != ''){
-                socket.emit('login',{uid:uid, username:participant})
-            }
-        }else{
-            let getUser = window.sessionStorage.getItem('userData');
-            var obj = JSON.parse(getUser)
-            setUid(obj.uid);
-            setParticipant(obj.username);
-            if(participant != ''){
-                socket.emit('login',{uid:uid, username:participant})
-            }
-        }
-         
-    }
- 
-    if(wb == "watch"){
-        return(
-            <div className="container">
-                <DataContext.Provider value={data}>
-                    <ViewContainer/>
-                </DataContext.Provider>
-            </div>
-        );
-    }else if(wb == "broadcast"){
-        return(
-            <div className="container">
-                <DataContext.Provider value={data}>
-                    <BroadcastContainer />
-                </DataContext.Provider>
-            </div>
-        );
-    }
- 
+  if (wb == "watch") {
+    return (
+      <div className="container">
+        <DataContext.Provider value={data}>
+          <ViewContainer />
+        </DataContext.Provider>
+      </div>
+    );
+  } else if (wb == "broadcast") {
+    socket.emit("resetStreamerTimeout");
+    return (
+      <div className="container">
+        <DataContext.Provider value={data}>
+          <BroadcastContainer />
+        </DataContext.Provider>
+      </div>
+    );
+  }
 }
