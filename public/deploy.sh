c336e04519d790669bda6d4c84e1de0ea4e2df90
Anna Suo-Anttila <anvuori@gmail.com>
2020-11-09 14:24:05 +0200

    Cert commented

--- public/deploy.sh
+++ public/deploy.sh
@@ -7,12 +7,12 @@ tar -zxvf deploy-branch.tar.gz
 rm deploy-branch.tar.gz
 
 cp reactcert/61619066_silverstream.dy.fi3000.cert silverstreaming-deploy-branch/cert.cert
-cp reactcert/61619066_silverstream.dy.fi3000.key silverstreaming-deploy-branch/cert.key
+cp reactcert/61619066_silverstream.dy.fi3000.key silverstreaming-deploy-branch/key.key
 
 
 cd silverstreaming-deploy-branch
 sudo docker build --tag node_server . -f Dockerfile_server
 sudo docker build --tag ui . -f Dockerfile_ui
 sudo docker run -d -p 4000:4000 -t node_server
-sudo docker run -d -p 3000:3000 -t ui -v ~/reactcert:/reactcert
+sudo docker run -d -p 3000:3000 -t ui
 
