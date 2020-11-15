84938609b83c69f788d79066457134d45fe3630f
Ville Mannila <ville.mannila@tuni.fi>
2020-11-15 21:19:54 +0200

    Sidebar looks and more buttons

--- src/Components/Nav/RightNav.js
+++ src/Components/Nav/RightNav.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+
 const Ul = styled.ul`
   list-style: none;
   display: flex;
@@ -10,14 +12,14 @@ const Ul = styled.ul`
   }
   @media (max-width: 5180px) {
     flex-flow: column nowrap;
-    background-color: #0D2538;
+    background-color: #9e9e9e;
     position: fixed;
     transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
     top: 0;
     right: 0;
     height: 100vh;
-    width: 300px;
-    padding-top: 3.5rem;
+    width: 250px;
+    padding-top: 2rem;
     transition: transform 0.3s ease-in-out;
     li {
       color: #fff;
@@ -25,20 +27,54 @@ const Ul = styled.ul`
   }
 `;
 
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: "white"};
+  color: "teal"};
+
+  font-size: 1em;
+  margin: 0.5em;
+  padding: 0.2em 1em;
+  border: 2px solid teal;
+  border-radius: 3px;
+`;
+
+const Select = styled.select`
+  /* Adapt the colors based on primary prop */
+  background: "white"};
+  color: "teal"};
+
+  font-size: 1em;
+  margin: 0em;
+  padding: 0.15em 1em;
+  border: 3px solid teal;
+  border-radius: 3px;
+`;
+
 const RightNav = ({ open }) => {
     return (
         <Ul open={open}>
-            <select> 
-                <option value="0">Select Video source</option>
-            </select>
-            <select> 
-                <option value="0">Select Audio source</option>
-            </select>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Sign In</li>
-            <li>Sign Up</li>
+            <p>Select Webcam</p>
+            <Select>
+                <option value="0">Select Webcam</option>
+            </Select>
+            <p>Select Microphone</p>
+            <Select>
+                <option value="0">Select Audio</option>
+            </Select>
+            <p />
+            <div>
+            <p>Participant view</p>
+                    <Button> Single </Button>
+                    <Button>2 x 2</Button>
+                    <Button>3 x 3</Button>
+                    <Button>4 x 4</Button>
+            </div>
+            <Button>Mute Microphone</Button>
+            <Button>Mute/Unmute Sound</Button>
+            <Button>Camera On/off</Button>
+            <Button>Copy Session Link</Button>
+            <Button>Leave Session</Button>
         </Ul>
     )
 }
