1229c698a19be97502f74f08b8caaaca7d4c3169
Anna Suo-Anttila <anvuori@gmail.com>
2020-11-29 22:28:58 +0200

    login/logout buttons

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Burger from './Burger';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 const Nav = styled.nav`
   width: 100%;
@@ -12,14 +13,59 @@ const Nav = styled.nav`
   .logo {
     padding: 15px 0;
   }
+  .g-signin2{
+    
+  }
+
 `
 
 const Navbar = () => {
+  const [signedIn, setSignedIn] = useState(false);
+  //const [name, setName] = useState(true);
+
+  const responseGoogleSuccess = (response) => {
+    setSignedIn(true)
+    console.log(response);
+  }
+  
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  function logout(){
+    setSignedIn(false)
+    console.log("Logged out");
+  }
+
+  function signInButton(){
+    if(signedIn===true){
+      return (
+        <GoogleLogout
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      >
+      </GoogleLogout>
+      )
+    }
+    else {
+      return (<GoogleLogin
+    clientId="307901483170-m56a98qcu5hjrtknsttovn1niepmdfn2.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogleSuccess}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+    prompt={'consent'}
+    />)
+    }
+  }
+
   return (
     <Nav>
+    {signInButton()}
       <div className="logo">
       </div>
       <Burger />
+      
     </Nav>
   )
 }
